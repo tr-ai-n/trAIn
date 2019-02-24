0c2eac988f6808bbe6a600feedce9e579b8d4ccc
sandipbhuyan <sandipbhuyan@gmail.com>
2019-02-25 03:11:09 +0530

    Add template

--- public/views.py
+++ public/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>This is the homepage</h1>')
+    template = loader.get_template('public/index.html')
+    page_info = {
+        "title" : "Home Page",
+        "Description" : "It is a free app for data learning developer"
+    }
+    return HttpResponse(template.render(page_info, request))
